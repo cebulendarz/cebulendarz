5643dd5db97fe9a1e6b52f25eaa9a464d4328d85
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2022-05-20 15:32:34 +0200

    Dodaje mui

--- apps/webapp/src/app/app.tsx
+++ apps/webapp/src/app/app.tsx
@@ -1,4 +1,7 @@
 import styled from '@emotion/styled';
+import CssBaseline from '@mui/material/CssBaseline';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import Button from '@mui/material/Button/Button';
 
 const Layout = styled.div`
   margin: 32px auto;
@@ -6,10 +9,27 @@ const Layout = styled.div`
   width: 600px;
 `;
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#b31536',
+    },
+    secondary: {
+      main: '#999999',
+    },
+  },
+});
+
 export const App = () => {
   return (
-    <Layout>
-      cebulendarz
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <>
+        <CssBaseline />
+        <Layout>
+          <div>cebulendarz</div>
+          <div><Button variant="outlined">klikaj mocno</Button></div>
+        </Layout>
+      </>
+    </ThemeProvider>
   );
 }
