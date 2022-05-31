51a41adea9276fc5776e94ceee8b3b3b20b5d73f
Grzegorz Lipecki <glipecki@consdata.pl>
2022-05-31 14:45:10 +0200

    Podbija nrwl/nx z 14.0.1 na 14.1.9

--- apps/webapp/jest.config.ts
+++ apps/webapp/jest.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   displayName: 'webapp',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
