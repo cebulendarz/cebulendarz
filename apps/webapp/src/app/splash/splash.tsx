8392540492e77376da0073b7c512d6b02b38f5d2
drolewski <drolewski@consdata.com>
2022-06-05 10:29:00 +0200

    Remove title from splash

--- apps/webapp/src/app/splash/splash.tsx
+++ apps/webapp/src/app/splash/splash.tsx
@@ -2,14 +2,12 @@ import styled from '@emotion/styled';
 import { Layout } from '../ui-elements/layout';
 import { Link as RouterLink } from 'react-router-dom';
 import { Button } from '@mui/material';
-import { useDocumentTitle } from '../document-title/use-document-title';
 
 const StyledRouterLink = styled(RouterLink)`
   text-decoration: none;
 `;
 
 export const Splash = () => {
-  useDocumentTitle('Stwórz wydarzenie');
   return (
     <Layout>
       <div>
