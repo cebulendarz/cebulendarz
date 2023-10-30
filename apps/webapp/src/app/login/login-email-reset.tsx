46b59a7f27497589c15d9b6b77435ccfc4dd66c2
Grzegorz Lipecki <grzegorrz.lipecki@gmail.com>
2023-10-30 16:26:52 +0100

    lint fixes

--- apps/webapp/src/app/login/login-email-reset.tsx
+++ apps/webapp/src/app/login/login-email-reset.tsx
@@ -26,6 +26,7 @@ export const LoginEmailReset = () => {
           'Link resetowania hasła zostały wysłany na podany adres email.'
         );
         console.info('Password reset link sent [email=%o]', email);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } catch (error: any) {
         console.error(`Error while authenticating`, error);
         if (error.code === 'auth/email-already-in-use') {
